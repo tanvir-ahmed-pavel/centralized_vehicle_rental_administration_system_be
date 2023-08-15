70474428b6383bb21d5c7f8db77b5b5662677f33
Tanvir Ahmed Pavel <pavelmeandexa720@gmail.com>
2023-08-15 14:13:35 +0600

    fixed readme

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
 Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+Route::get('/test', function (){
+    return "working";
+});
